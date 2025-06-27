18b269b1cdd74868776a31cb175b3aaa2508fd27
DamodaraBarbosa <damodarabarbosa@gmail.com>
2025-06-27 14:21:07 -0300

    Ajustes no cast do arquivo stg_orders_northwind

--- dbt_exercise/models/staging/stg_orders_northwind.sql
+++ dbt_exercise/models/staging/stg_orders_northwind.sql
@@ -1,21 +1,21 @@
 with orders as (
     select *
-    from {{ source('raw', 'orders') }}
+    from {{ source('northwind', 'orders') }}
 )
 
 select
-    cast(order_id, string) as order_id
-    , cast(customer_id, string) as customer_id
-    , cast(employee_id, string) as employee_id
-    , cast(order_date, date) as order_date
-    , cast(required_date, date) as required_date
-    , cast(shipped_date, date) as shipped_date
-    , cast(ship_via, string) as ship_via
-    , cast(freight, decimal(10, 2)) as freight
-    , cast(ship_name, string) as ship_name
-    , cast(ship_address, string) as ship_address
-    , cast(ship_city, string) as ship_city
-    , cast(ship_region, string) as ship_region
-    , cast(ship_postal_code, string) as ship_postal_code
-    , cast(ship_country, string) as ship_country
+    cast(order_id as string) as order_id
+    , cast(customer_id as string) as customer_id
+    , cast(employee_id as string) as employee_id
+    , cast(order_date as date) as order_date
+    , cast(required_date as date) as required_date
+    , cast(shipped_date as date) as shipped_date
+    , cast(ship_via as string) as ship_via
+    , cast(freight as numeric) as freight
+    , cast(ship_name as string) as ship_name
+    , cast(ship_address as string) as ship_address
+    , cast(ship_city as string) as ship_city
+    , cast(ship_region as string) as ship_region
+    , cast(ship_postal_code as string) as ship_postal_code
+    , cast(ship_country as string) as ship_country
 from orders
