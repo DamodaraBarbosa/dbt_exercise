58b8c5c042909e45ebebc618962d7ef4abe46502
DamodaraBarbosa <damodarabarbosa@gmail.com>
2025-06-27 16:25:50 -0300

    Adicao do arquivo stg_employee_territories

---
+++ dbt_exercise/models/staging/stg_employees_territories.sql
@@ -0,0 +1,9 @@
+with emplyoee_ territories as (
+    select *
+    from {{ source('northwind', 'employee_territories') }}
+)
+
+select
+    cast(employee_id as string) as employee_id
+    , cast(territory_id as string) as territory_id
+from employee_territories
