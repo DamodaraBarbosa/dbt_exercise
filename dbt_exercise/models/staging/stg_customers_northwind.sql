e6cdd5a34c42e0a100c1417e84c173099d4eeb63
DamodaraBarbosa <damodarabarbosa@gmail.com>
2025-06-27 15:19:11 -0300

    Adicao do arquivo stg_employees_northwind

---
+++ dbt_exercise/models/staging/stg_customers_northwind.sql
@@ -0,0 +1,18 @@
+with customers as (
+    select *
+    from {{ source('northwind', 'customers') }}
+)
+
+select
+    cast(customer_id as string) as customer_id
+    , cast(company_name as string) as company_name
+    , cast(contact_name as string) as contact_name
+    , cast(contact_title as string) as contact_title
+    , cast(address as string) as address
+    , cast(city as string) as city
+    , cast(region as string) as region
+    , cast(postal_code as string) as postal_code
+    , cast(country as string) as country
+    , cast(phone as string) as phone
+    , cast(fax as string) as fax
+from customers
