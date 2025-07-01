0835ff557b0411b9f32f6c8b48de94ac3e5aa69d
DamodaraBarbosa <damodarabarbosa@gmail.com>
2025-07-01 13:35:54 -0300

    Adicao da coluna flag is_shipped

--- dbt_exercise/models/intermediate/int_orders_northwind.sql
+++ dbt_exercise/models/intermediate/int_orders_northwind.sql
@@ -16,7 +16,11 @@ with orders as (
         , orders.order_date
         , orders.required_date
         , orders.shipped_date
-        , date_diff('day', orders.order_date, orders.shipped_date) as days_to_ship
+        , date_diff(orders.shipped_date, orders.order_date, day) as days_to_ship
+        , case 
+            when days_to_ship is null then False
+            else True
+        end as is_shipped        
         , orders.ship_via
         , sum(order_details.unit_price * order_details.quantity * (1 - order_details.discount)) as total_order_amount
         , orders.freight
@@ -28,6 +32,20 @@ with orders as (
     from orders
     left join order_details
         on orders.order_id = order_details.order_id
+    group by 
+        orders.order_id
+        , orders.customer_id
+        , orders.employee_id
+        , orders.order_date
+        , orders.required_date
+        , orders.shipped_date
+        , orders.ship_via
+        , orders.freight
+        , orders.ship_name
+        , orders.ship_address
+        , orders.ship_city
+        , orders.ship_region
+        , orders.ship_country
 )
 
 select *
