b9467d8d6c26058e9d88b2cae826a1d499b13893
DamodaraBarbosa <damodarabarbosa@gmail.com>
2025-09-06 16:54:40 -0300

    Ajustando o intervalo de datas para 01-01-2008 e 31-12-2030

--- dbt_exercise/models/marts/dim_calendar.sql
+++ dbt_exercise/models/marts/dim_calendar.sql
@@ -2,7 +2,7 @@
 with dates as (
     {{ dbt_utils.date_spine(
         datepart="day",
-        start_date="cast('2020-01-01' as date)",
+        start_date="cast('2008-01-01' as date)",
         end_date="cast('2030-12-31' as date)"
     ) }}
 )
